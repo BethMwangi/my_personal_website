f164f9cf29b1c247b61b8ba708908ee5523da7e8
BethMwangi <wanjikumwangib@gmail.com>
2016-04-16 02:49:13 +0300

    added a sticky navigation bar

--- js/app.js
+++ js/app.js
@@ -1,12 +1,41 @@
-$(document).foundation();
+//$(document).foundation();
+//
+//$('a[href^="#"]').on('click', function (event) {
+//    var target = $($(this).attr('href'));
+//
+//    if (target.length) {
+//        event.preventDefault();
+//        $('html, body').animate({
+//            scrollTop: target.offset().top
+//        }, 1000);
+//    }
+//})
+$(function () {
+    var sticky_navigation_offset_top = $('#header').offset().top;
+    var sticky_navigation = function () {
+        var scroll_top = $(window).scrollTop();
+        if (scroll_top > sticky_navigation_offset_top) {
+            $('#header').css({
+                'position': 'fixed',
+                'top': 0,
+                'left': 0
+            });
+        } else {
+            $('#header').css({
+                'position': 'relative'
+            });
+        }
+    };
 
-$('a[href^="#"]').on('click', function (event) {
-    var target = $($(this).attr('href'));
+    // run our function on load
+    sticky_navigation();
 
-    if (target.length) {
+    // and run it again every time you scroll
+    $(window).scroll(function () {
+        sticky_navigation();
+    });
+    $('a[href="#"]').click(function (event) {
         event.preventDefault();
-        $('html, body').animate({
-            scrollTop: target.offset().top
-        }, 1000);
-    }
-})
+    });
+
+});
